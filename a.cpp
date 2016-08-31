d9684c668900f0ad3ab456caad9101ce0f0503b3
Vaishnavi Gadekar <vaishnavi.gadekar@cumminscollege.in>
2016-08-31 08:52:35 +0530

    After cloning
    Cloning means copying the multiple files in the repository from web
    application into the desktop application

--- a.cpp
+++ a.cpp
@@ -3,3 +3,5 @@ I am excited about learning how to create projects in GitHub.
 We are doing this in ESDL lab
 This is the new line I have added during editing the file.
 This is written after I created a new branch A-3325
+
+The repository has been cloned and after cloning changes are been made to the file.
